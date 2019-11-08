c39b8640ff5aed166dac20ad71a26845cc0a8dcb
ThiccSoggyBread <denleyng1@gmail.com>
2019-11-08 20:17:27 +0800

    Revert "2 Player Inputs"
    This reverts commit 0ed0f778dfdc61cdf508cde2ea4f4754854dc5d8.

--- JCUStrawberryJam/JCUJamJam/Assets/Scripts/PlayerController.cs
+++ JCUStrawberryJam/JCUJamJam/Assets/Scripts/PlayerController.cs
@@ -4,65 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Player ID to determine which player")]
-    [SerializeField]
-    [Range(1,2)]
-    int playerID; // Player 1 WASD, Player 2 Arrow Key
-
-    [Header("Player Speed")]
-    [SerializeField]
-    float playerSpeed;
-
-    Vector3 playerInput;
-    Rigidbody rgdbdy;
-    bool isGrounded = false;
-
-
     // Start is called before the first frame update
     void Start()
     {
-        rgdbdy = transform.GetComponent<Rigidbody>();
-
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        // playerControls
-        PlayerControls();
-    }
-
-
-    private void FixedUpdate()
-    {
-        rgdbdy.MovePosition(rgdbdy.position + playerInput * playerSpeed * Time.fixedDeltaTime);
-    }
-
-
-    void PlayerControls()
-    {
-        playerInput = Vector3.zero;
-        if (playerID == 1)
-        {
-            playerInput.x = Input.GetAxis("Horizontal");
-            playerInput.z = Input.GetAxis("Vertical");
-        }
-
-        if(playerID == 2)
-        {
-            playerInput.x = Input.GetAxis("Horizontal2");
-            playerInput.z = Input.GetAxis("Vertical2");
-        }
-
-
-    }
-
-    public void OnCollisionEnter(Collision col)
-    {
-        if(col.gameObject.tag == "ground")
-        {
-            isGrounded = true;
-        }
+        
     }
 }
