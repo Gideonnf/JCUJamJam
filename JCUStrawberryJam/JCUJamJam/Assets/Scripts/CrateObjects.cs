feadb7f20d5f7ef6e3c4ee95288381ee88450a0a
gideon <gideonnfrancis999@gmail.com>
2019-11-09 06:10:12 +0800

    Edited interactables so its easier to make copies of them

---
+++ JCUStrawberryJam/JCUJamJam/Assets/Scripts/CrateObjects.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateObjects : InteractbleObjects
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void CollisionInteraction(PlayerController playerController, InteractbleObjects.CollisionState colState)
+    {
+        base.CollisionInteraction(playerController, colState);
+        if (Input.GetKey(KeyCode.E))
+        {
+            if (playerController.GetPlayerID() == 1)
+            {
+                playerController.pickObject(this.gameObject);
+            }
+        }
+    }
+
+}
--- JCUStrawberryJam/JCUJamJam/Assets/Scripts/InteractbleObjects.cs
+++ JCUStrawberryJam/JCUJamJam/Assets/Scripts/InteractbleObjects.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class InteractbleObjects : MonoBehaviour
 {
-    
+    public enum CollisionState
+    {
+        ENTER,
+        STAY,
+        EXIT
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +23,25 @@ public class InteractbleObjects : MonoBehaviour
         
     }
 
+    public virtual void CollisionInteraction(PlayerController playerController, CollisionState colState)
+    {
+
+    }
+
+    public virtual void TriggerInteraction(PlayerController playerController, CollisionState colState)
+    {
+
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        
+        PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+        // Check if player controller exist in the game object
+        if (playerController)
+        {
+            // If it does check for player 2
+            CollisionInteraction(playerController, CollisionState.ENTER);
+        }
     }
 
     private void OnCollisionStay(Collision collision)
@@ -30,13 +51,45 @@ public class InteractbleObjects : MonoBehaviour
         if (playerController)
         {
             // If it does check for player 2
-            if(Input.GetKey(KeyCode.E))
-            {
-                if (playerController.GetPlayerID() == 1)
-                {
-                    playerController.pickObject(this.gameObject);
-                }
-            }
+            CollisionInteraction(playerController, CollisionState.STAY);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+        // Check if player controller exist in the game object
+        if (playerController)
+        {
+            // If it does check for player 2
+            CollisionInteraction(playerController, CollisionState.EXIT);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
+        if (playerController)
+        {
+            TriggerInteraction(playerController, CollisionState.ENTER);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
+        if (playerController)
+        {
+            TriggerInteraction(playerController, CollisionState.STAY);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
+        if (playerController)
+        {
+            TriggerInteraction(playerController, CollisionState.EXIT);
         }
     }
 }
