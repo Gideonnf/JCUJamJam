21bb1aab0b469573770c4956e9d55255c6f5fa48
ThiccSoggyBread <denleyng1@gmail.com>
2019-11-08 22:11:24 +0800

    Added "Rotation" & Base NavMesh

--- JCUStrawberryJam/JCUJamJam/Assets/Scripts/PlayerController.cs
+++ JCUStrawberryJam/JCUJamJam/Assets/Scripts/PlayerController.cs
@@ -11,11 +11,14 @@ public class PlayerController : MonoBehaviour
 
     [Header("Player Speed")]
     [SerializeField]
-    float playerSpeed;
+    float pMoveSpeed;
+    float pRotationSpeed = 1f;
 
     Vector3 playerInput;
+    float playerRotInput;
     Rigidbody rgdbdy;
     bool isGrounded = false;
+    bool holdFlare = false;
 
 
     // Start is called before the first frame update
@@ -30,29 +33,76 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // playerControls
+        
+
+        //if (playerRotInput != 0)
+        //    transform.Rotate(0, playerRotInput * 30 * Time.deltaTime, 0);
+        if (playerRotInput != 0)
+        {
+            Vector3 yRot = new Vector3(0, playerRotInput, 0);
+            yRot = yRot.normalized * pRotationSpeed;
+            Quaternion deltaRot = Quaternion.Euler(yRot);
+            rgdbdy.MoveRotation(rgdbdy.rotation * deltaRot);
+            //if (playerRotInput > 0)
+            //    transform.Rotate(0, pRotationSpeed, 0);
+            //else
+            //    transform.Rotate(0, -pRotationSpeed, 0);
+        }
+        else
+        {
+            rgdbdy.transform.Rotate(0, 0, 0);
+        }
+
         PlayerControls();
     }
 
 
     private void FixedUpdate()
     {
-        rgdbdy.MovePosition(rgdbdy.position + playerInput * playerSpeed * Time.fixedDeltaTime);
+
+
+
+        // rgdbdy.MovePosition(rgdbdy.position + playerInput * pMoveSpeed * Time.fixedDeltaTime);
+        Vector3 movement = transform.rotation * Vector3.forward;
+        if (playerInput.z > 0)
+            rgdbdy.MovePosition(rgdbdy.position + movement * pMoveSpeed * Time.fixedDeltaTime);
+        else if(playerInput.z < 0)
+            rgdbdy.MovePosition(rgdbdy.position + -movement * pMoveSpeed * Time.fixedDeltaTime);
+
+
+        // var target = Quaternion.Euler(0, playerRotInput * 30, 0);
+
+        
     }
 
 
     void PlayerControls()
     {
         playerInput = Vector3.zero;
+        playerRotInput = 0;
         if (playerID == 1)
         {
-            playerInput.x = Input.GetAxis("Horizontal");
+            //playerInput.x = Input.GetAxis("Horizontal");
+            playerRotInput = Input.GetAxis("Horizontal");
+            //if (Input.GetKeyDown(KeyCode.A))
+            //    transform.Rotate(0, -pRotationSpeed, 0);
+           
             playerInput.z = Input.GetAxis("Vertical");
+            if (Input.GetKeyDown(KeyCode.E))
+                holdFlare = true;
+            else if (Input.GetKeyUp(KeyCode.E))
+                holdFlare = false;
         }
 
         if(playerID == 2)
         {
-            playerInput.x = Input.GetAxis("Horizontal2");
+            //playerInput.x = Input.GetAxis("Horizontal2");
+            playerRotInput = Input.GetAxis("Horizontal2");
             playerInput.z = Input.GetAxis("Vertical2");
+            if (Input.GetKeyDown(KeyCode.KeypadEnter))
+                holdFlare = true;
+            else if (Input.GetKeyUp(KeyCode.KeypadEnter))
+                holdFlare = false;
         }
 
 
